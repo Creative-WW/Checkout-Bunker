4fde08043d38d1524c2a0cb46cf9d6ae41f48ba2
Creative-WW <creative@wwdcomplete.com>
2025-07-16 15:52:58 -0400

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-window.projectConfig = {
-  title: "DQ8664 . Checkout Bunker",
-  description: "View the Checkout Bunker in AR on your mobile device.",
-  image: "assets/Checkout Rack.png", // used for AR button
-  usdz: "assets/AR_DQ8664.usdz",
-  glb: "assets/AR_DQ8664.glb",
-  logo: "assets/dq_logo.png" // company logo
-};

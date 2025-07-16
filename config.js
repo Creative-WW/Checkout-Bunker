2ce477e24f3383d0ca30b4a7a23583d55499892a
Creative-WW <creative@wwdcomplete.com>
2025-07-16 15:24:34 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // config.js
 window.projectConfig = {
-  title: "DQ3358 . Q Rack",
-  description: "View the Q Rack in AR on your mobile device.",
-  image: "assets/Q Rack.png", // used for AR button
-  usdz: "assets/AR_DQ3358.usdz",
-  glb: "assets/AR_DQ3358.glb",
+  title: "DQ8664 . Checkout Bunker",
+  description: "View the Checkout Bunker in AR on your mobile device.",
+  image: "assets/Checkout Rack.png", // used for AR button
+  usdz: "assets/AR_DQ8664.usdz",
+  glb: "assets/AR_DQ8664.glb",
   logo: "assets/dq_logo.png" // company logo
 };

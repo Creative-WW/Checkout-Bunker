f5ab2c95deebc383d80839ec5066bcf8940b0212
Creative-WW <creative@wwdcomplete.com>
2025-07-17 14:33:39 -0400

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ window.projectConfig = {
   title: "DQ8664 . Checkout Bunker",
   description: "View the Checkout Bunker in AR on your mobile device.",
   image: "assets/Checkout Rack.png",
-  usdz: "assets/AR_DQ8664.usdz", // relative path for iOS Quick Look
-  glb: "https://creative-ww.github.io/Q-Rack/assets/AR_DQ8664.glb", // HTTPS URL for Android Scene Viewer + model-viewer
+  usdz: "assets/AR_DQ8664.usdz",
+  glb: "https://creative-ww.github.io/Q-Rack/assets/AR_DQ8664.glb",
   logo: "assets/dq_logo.png"
 };
